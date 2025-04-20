ec8ad15ec888d3272f17ed7d02bd4b70e9387b4c
Tomar1907 <soumyatomar1908@gmail.com>
2025-04-20 12:45:29 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q="
-const searchBox = document.querySelector(".search input")
-const searchBtn = document.querySelector(".search button")
-const weatherIcon = document.querySelector(".weather-icon")
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
 
 async function checkWeather(city) {
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
